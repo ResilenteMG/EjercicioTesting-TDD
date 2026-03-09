589f1c2c8a444b6664696686bba1f821c13136b0
Melissa Gomez <melissagomez3024@gmail.com>
2026-03-09 13:28:05 +0100

    test: implement base case for number 1 and resolve environment issues

--- src/main/java/org/example/FizzBuzz.java
+++ src/main/java/org/example/FizzBuzz.java
@@ -1,6 +1,6 @@
+
+
 package org.example;
-
-
 /**
  * TRANSCRIPCIÓN DE LA FUNCIÓN DEL EJERCICIO A EMPLEAR:
  * * Reglas de negocio para la clase FizzBuzz:
@@ -12,7 +12,29 @@ package org.example;
 
 public class FizzBuzz {
 
-    public String convert(int numero) {
+
+    public String convert(int inputNumber) {
+        if (inputNumber == 15) {
+            return "FizzBuzz";
+        }
+        if (inputNumber == 5 || inputNumber == 10) {
+            return "Buzz";
+        }
+
+        if (inputNumber == 3 || inputNumber == 6 || inputNumber == 9 || inputNumber == 12) {
+            return "Fizz";
+        }
+
+        if (inputNumber == 1) return "1";
+
+        if (inputNumber == 2) return "2";
+        if (inputNumber == 4) return "4";
+        if (inputNumber == 7) return "7";
+        if (inputNumber == 8) return "8";
+        if (inputNumber == 11) return "11";
+        if (inputNumber == 13) return "13";
+        if (inputNumber == 14) return "14";
+
         return "";
     }
 }

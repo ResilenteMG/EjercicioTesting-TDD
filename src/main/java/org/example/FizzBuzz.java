ef49f834a6c9615d512e8dc165b80d3c0cab9d48
Melissa Gomez <melissagomez3024@gmail.com>
2026-03-09 07:47:22 +0100

    fix: relocate FizzBuzz class to correct package and fix syntax errors

---
+++ src/main/java/org/example/FizzBuzz.java
@@ -0,0 +1,18 @@
+package org.example;
+
+
+/**
+ * TRANSCRIPCIÓN DE LA FUNCIÓN DEL EJERCICIO A EMPLEAR:
+ * * Reglas de negocio para la clase FizzBuzz:
+ * - Si el número es múltiplo de 3 -> Retorna "Fizz".
+ * - Si el número es múltiplo de 5 -> Retorna "Buzz".
+ * - Si el número es múltiplo de 3 y 5 -> Retorna "FizzBuzz".
+ * - En cualquier otro caso -> Retorna el número original.
+ */
+
+public class FizzBuzz {
+
+    public String convert(int numero) {
+        return "";
+    }
+}
